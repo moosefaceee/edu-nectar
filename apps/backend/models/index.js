0527592cfc213fcbd57c178533fa2ceda4de88ee
George van Heerden <george.vanheerden@sovtech.com>
2023-06-03 00:39:42 +0200

    feature(api): added generate endpoint to analyse pdf and write data to db, exposed endpoints to fetch data

--- apps/backend/models/index.js
+++ apps/backend/models/index.js
@@ -1,1 +1,4 @@
 export { default as TestModel } from './test.js'
+export { default as QuizModel } from './quiz.js'
+export { default as SummaryModel } from './summary.js'
+export { default as TopicsModel } from './topics.js'
